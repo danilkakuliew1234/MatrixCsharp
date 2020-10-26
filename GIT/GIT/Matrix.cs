9a1a476c659c88647b88cdf3fb4434aba967865e
Daniel <danilkakuliew@gmail.com>
2020-10-26 11:08:42 +0300

    Creating class Matrix

---
+++ GIT/GIT/Matrix.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GIT
+{
+    class Matrix
+    {
+        public int[,] CreateMatrix(int rows = 1, int cols = 1, int minNumber = -5, int maxNumber = 5)
+        {
+            int[,] Matrix = new int[rows, cols];
+            Random RandomNumber = new Random();
+            for (int i = 0; i < Matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < Matrix.GetLength(1); j++)
+                {
+                    Matrix[i, j] = RandomNumber.Next(minNumber, maxNumber);
+                }
+            }
+            return Matrix;
+        }
+        public void DrawMatrix(int[,] Matrix)
+        {
+            for (int i = 0; i < Matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < Matrix.GetLength(1); j++)
+                {
+                    Console.Write(Matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}

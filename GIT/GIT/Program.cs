529555298f27043d7076666af05d0faf192eb89c
Daniel <danilkakuliew@gmail.com>
2020-10-26 13:17:21 +0300

    Test methods MultiplyMatrix() and TakeAwayMatrix()

--- GIT/GIT/Program.cs
+++ GIT/GIT/Program.cs
@@ -9,28 +9,23 @@ namespace GIT
         {
             Matrix matrix = new Matrix();
 
-            Console.WriteLine();
-            int[,] MatrixOne = matrix.CreateMatrix(5, 5, -10, 10);
+            
+            int[,] MatrixOne = matrix.CreateMatrix(3, 3, -10, 10);
             matrix.DrawMatrix(MatrixOne);
-
             Console.WriteLine();
-            int[,] MatrixTwo = matrix.CreateMatrix(5, 5, -10, 10);
+
+            int[,] MatrixTwo = matrix.CreateMatrix(3, 3, -10, 10);
             matrix.DrawMatrix(MatrixTwo);
+            Console.WriteLine("\nMultiply");
 
+            int[,] MultiplyMatrix = matrix.MultiplyMatrix(MatrixOne, MatrixTwo);
+            matrix.DrawMatrix(MultiplyMatrix);
+            Console.WriteLine("\nTake Away");
+
+            int[,] TakeAway = matrix.TakeAwayMatrix(MatrixOne, MatrixTwo);
+            matrix.DrawMatrix(TakeAway);
             Console.WriteLine();
-            int[,] summ = matrix.SummMatrix(MatrixOne, MatrixTwo);
-            matrix.DrawMatrix(summ);
 
-            DataBase dataBase = new DataBase();
-            try
-            {
-                dataBase.ConnectDataBase("sql2371754", "bV9!vX4%", "3306", "sql2.freesqldatabase.com", "code");
-                Console.WriteLine("Connection opened");
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
         }
     }
 }

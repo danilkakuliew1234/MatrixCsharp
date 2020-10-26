635fd5bb679cce0277b526e47ea6ad97fa4e0098
Daniel <danilkakuliew@gmail.com>
2020-10-26 11:01:22 +0300

    createMatrix

--- GIT/GIT/Program.cs
+++ GIT/GIT/Program.cs
@@ -6,7 +6,17 @@ namespace GIT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] Matrix = new int[5, 5];
+            Random randomNumber = new Random();
+            for(int i = 0; i < Matrix.GetLength(0); i++)
+            {
+                for(int j = 0; j < Matrix.GetLength(1); j++)
+                {
+                    Matrix[i, j] = randomNumber.Next(0, 10);
+                    Console.Write(Matrix[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }

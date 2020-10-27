0a7291556af9cb86f70d2264dab0605335a5b86b
Daniel <danilkakuliew@gmail.com>
2020-10-27 09:51:33 +0300

    Working..task1

--- GIT/GIT/Program.cs
+++ GIT/GIT/Program.cs
@@ -10,7 +10,36 @@ namespace GIT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Tasks.Tasks.task1(45));
+            Console.WriteLine(task1());
+        }
+        public static float task1()
+        {
+            int i = 1;
+            int _exp = 2;
+            int degree = 2;
+            int z = -1;
+            float answer = 1 / (i * _exp) * z;
+            float lasterror;
+            float summ = answer;
+
+            do
+            {
+                lasterror = answer;
+                answer = 1 / (float)(Math.Pow(i, 2) * Math.Pow(_exp, degree));
+                summ += answer;
+                degree++;
+            } while (Math.Abs(lasterror - answer) > 0.001);
+
+            return summ;
+        }
+        public static int factorial(int iterator)
+        {
+            int summ = 1;
+            for(int i = 1; i <= iterator; i++)
+            {
+                summ *= i;
+            }
+            return summ;
         }
     }
 }

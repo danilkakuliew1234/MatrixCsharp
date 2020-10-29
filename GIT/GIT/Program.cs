1caee3cfe54fe3385d8813c5003cbe1313caf480
Daniel <danilkakuliew@gmail.com>
2020-10-29 09:47:08 +0300

    Created method task2 and factorial

--- GIT/GIT/Program.cs
+++ GIT/GIT/Program.cs
@@ -10,7 +10,7 @@ namespace GIT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(task1());
+            Console.WriteLine(task2(10));
         }
         public static float task1()
         {
@@ -32,5 +32,36 @@ namespace GIT
 
             return summ;
         }
+        public static double task2(int inputX)
+        {
+            double answer = 0, summ = 0;
+            int numerator = 3, degree = 3, z = 1;
+
+            answer = inputX - Math.Pow(inputX, degree) * z / factorial(numerator);
+            summ += answer;
+            degree = 5;
+            numerator = 5;
+
+            while (answer > 0.001)
+            {
+                answer = Math.Pow(inputX, degree) * z / factorial(numerator);
+                summ += answer;
+                degree += 2;
+                numerator += 2;
+                z = -1;
+            }
+            return summ;
+        }
+
+        public static int factorial(int n)
+        {
+            int _factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                _factorial *= i;
+            }
+            return _factorial;
+        }
+
     }
 }
